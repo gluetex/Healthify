fec16763d7cc1f772f9cc6facb479c1de4f3a7a0
gluetex <emre.ertugg1@gmail.com>
2023-07-28 16:56:53 +0300

    registerDBUserCreation

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -4,10 +4,12 @@ import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import {
   Auth,
-  signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  signOut
 } from "@angular/fire/auth";
+import { getFirestore } from 'firebase/firestore';
+import { setDoc, doc, collection } from "firebase/firestore";
+
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-register',
@@ -18,7 +20,26 @@ export class RegisterPage implements OnInit {
 
   email!: string;
   password!: string;
-  constructor(private router: Router, private toastController: ToastController, private navCtrl: NavController, private auth: Auth) { }
+  private db: any;
+
+
+  constructor(private router: Router,
+     private toastController: ToastController, 
+     private navCtrl: NavController, private auth: Auth,
+     ) { 
+      const firebaseConfig = {
+        apiKey: "AIzaSyAhwUQ4dyWspAU53ErwArgyXhl-ZzZNw2I",
+        authDomain: "healthify-78015.firebaseapp.com",
+        projectId: "healthify-78015",
+        storageBucket: "healthify-78015.appspot.com",
+        messagingSenderId: "307872925519",
+        appId: "1:307872925519:web:74d67cc5cbf3091704a73a",
+        measurementId: "G-09F20RSR0T"    };
+
+      const app = initializeApp(firebaseConfig);
+      this.db = getFirestore(app);
+
+     }
 
   isToastOpen=false;
   showSuccessToast = false;
@@ -26,19 +47,26 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
- 
-  async registerAction(){
-
+  async registerAction() {
     const user = await createUserWithEmailAndPassword(
       this.auth,
       this.email,
       this.password
-      );
-      this.presentToast('Signup success');
-      this.navCtrl.navigateForward('/login');
+    );
+  
+    const userDetail = {
+      email: this.email,
+    };
+  
+    const emailId = this.email.split('@')[0];  // This will split the email by '@' and take the first part.
+    const userRef = doc(this.db, "users", emailId);
+    const res = await setDoc(userRef, userDetail);
+  
+    this.presentToast('Signup success');
+    this.navCtrl.navigateForward('/login');
     return user;
-   
   }
+  
 
   redirectAction(){
     this.navCtrl.navigateForward('/login');
